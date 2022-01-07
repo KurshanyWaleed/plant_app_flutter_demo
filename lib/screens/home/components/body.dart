a29ffaf83bff935cab569eb5f49b435cfb896f69
waleed <kurshany.waleed@gmail.com>
2022-01-07 18:03:51 +0100

    first.3 adding titles  commit

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:palnt_app_flutter/constants.dart';
+
+import 'header_box_search.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -8,84 +9,51 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          height: size.height * .21,
-          color: Colors.white,
-          child: Stack(
-            children: [
-              Container(
-                // alignment: Alignment.center,
-                height: size.height * .2 - 27,
-                padding: const EdgeInsets.only(
-                    bottom: 25 + kDefaultPadding, right: 10, left: 10),
-                // padding: const EdgeInsets.symmetric(
-                //     horizontal: kDefaultPadding, vertical: 30),
-                decoration: BoxDecoration(
-                    color: customGreen,
-                    borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(36),
-                        bottomRight: Radius.circular(36))),
-                child: Row(
-                  children: [
-                    Text(
-                      'Hi Waleed ! ',
-                      style: Theme.of(context).textTheme.headline5!.copyWith(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                    const Spacer(),
-                    Image.asset(
-                      "assets/images/logo.png",
-                      width: 50,
-                      height: 50,
-                    )
-                  ],
-                ),
-              ),
-              Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 54,
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                              offset: const Offset(0, 10),
-                              blurRadius: 50,
-                              color: customGreen.withOpacity(0.23))
-                        ],
-                        color: Colors.white),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            onChanged: (value) => {},
-                            decoration: InputDecoration(
-                              hintText: "Search",
-                              hintStyle:
-                                  TextStyle(color: customGreen.withOpacity(.5)),
-                              enabledBorder: InputBorder.none,
-                              focusedBorder: InputBorder.none,
-                              //suffixIcon: const Icon(Icons.search)
-                            ),
-                          ),
-                        ),
-                        SvgPicture.asset("assets/icons/search.svg")
-                      ],
-                    ),
-                  ))
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithBoxSearch(size: size),
+          Row(
+            children: const [
+              TitleWithCustomUnderLine(text: "Recomended"),
             ],
           ),
-        )
-      ],
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderLine extends StatelessWidget {
+  const TitleWithCustomUnderLine({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text("$text",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(left: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/home/components/header_box_search.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../constants.dart';
+
+class HeaderWithBoxSearch extends StatelessWidget {
+  const HeaderWithBoxSearch({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 20),
+      height: size.height * .21,
+      color: Colors.white,
+      child: Stack(
+        children: [
+          Container(
+            // alignment: Alignment.center,
+            height: size.height * .2 - 27,
+            padding: const EdgeInsets.only(
+                bottom: 25 + kDefaultPadding, right: 10, left: 10),
+            // padding: const EdgeInsets.symmetric(
+            //     horizontal: kDefaultPadding, vertical: 30),
+            decoration: BoxDecoration(
+                color: customGreen,
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(36),
+                    bottomRight: Radius.circular(36))),
+            child: Row(
+              children: [
+                Text(
+                  'Hi Waleed ! ',
+                  style: Theme.of(context).textTheme.headline5!.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                const Spacer(),
+                Image.asset(
+                  "assets/images/logo.png",
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                height: 54,
+                margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: const Offset(0, 10),
+                          blurRadius: 50,
+                          color: customGreen.withOpacity(0.23))
+                    ],
+                    color: Colors.white),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) => {},
+                        decoration: InputDecoration(
+                          hintText: "Search",
+                          hintStyle:
+                              TextStyle(color: customGreen.withOpacity(.5)),
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          //suffixIcon: const Icon(Icons.search)
+                        ),
+                      ),
+                    ),
+                    SvgPicture.asset("assets/icons/search.svg")
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
