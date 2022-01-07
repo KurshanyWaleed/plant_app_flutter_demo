93fc5dfad689748f93157edc2a81c4cfc1ed4338
waleed <kurshany.waleed@gmail.com>
2022-01-07 16:48:36 +0100

    first.2 home_screen commit

---
+++ lib/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// Colors that we use in our app
+const kPrimaryColor = Color(0xFF0C9869);
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
+Color c = Colors.red;
+MaterialColor customGreen = const MaterialColor(0xFF0C9869, {
+  50: Color(0xffe8f5e9),
+  100: Color(0xffc8e6c9),
+  200: Color(0xffa5d6a7),
+  300: Color(0xff81c784),
+  400: Color(0xff66bb6a),
+  500: Color(0xFF0C9869),
+  600: Color(0xff43a047),
+  700: Color(0xff388e3c),
+  800: Color(0xff2e7d32),
+  900: Color(0xff1b5e20),
+});
---
+++ lib/screens/home/components/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:palnt_app_flutter/screens/home/components/body.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: builAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar builAppBar() {
+    return AppBar(
+      title: const Text('Plant App'),
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset('assets/icons/menu.svg'),
+      ),
+      centerTitle: true,
+    );
+  }
+}
